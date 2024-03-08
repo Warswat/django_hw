9ad7a10522de4c14cfed8ae34725708d9e040f98
Warswat <warswat228@gmail.com>
2024-03-08 10:07:30 +0300

    1.2 homework

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,27 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
+from pagination.settings import BUS_STATION_CSV
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
-
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
+    with open(BUS_STATION_CSV, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        stantions = []
+        for row in reader:
+            stantions.append({'Name': row.get('Name'), 'Street': row.get('Street'), 'District': row.get('District')})
+    page_number = int(request.GET.get("page", 1))
+    paginator = Paginator(stantions, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+         'bus_stations': page,
+         'page': page,
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,20 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def get_recipe(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    ingredients = DATA.get(dish).copy()
+
+    for key in ingredients:
+        ingredients[key] = DATA.get(dish)[key] * servings
+
+    context = {
+        'recipe': ingredients,
+        'servings': servings
+    }
+
+    return render(request, 'calculator/index.html', context)
+
